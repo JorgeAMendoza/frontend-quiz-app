9a689a4faa423b21199589570a6e0917212f22de
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-01-23 18:09:37 -0600

    cypress config and `page-load.cy.ts` test
    Fix cypress configuration error, create the `ThemeToggle` placeholder
    for test, update page title, and refactor test.

--- cypress/e2e/page-load.cy.ts
+++ cypress/e2e/page-load.cy.ts
@@ -10,31 +10,46 @@ describe('page load', () => {
     cy.get('h1').should('contain', 'Welcome to the Frontend Quiz!')
     cy.get('[data-cy="themeToggle"]').should('exist')
 
-    cy.get('[data-cy="quiz-options"]').should('have.length', 4)
+    cy.get('[data-cy="testList"]').children().should('have.length', 4)
   })
 
-  it('clicking on the quiz option navigates to the quiz page', () => {
-    cy.get('[data-cy="quiz-options"]')
+  it('clicking on the "HTML" quiz option navigates to the quiz page', () => {
+    cy.get('[data-cy="testList"]')
       .find('a')
       .then((links) => {
         cy.wrap(links[0]).should('contain', 'HTML')
         cy.wrap(links[0]).click()
-        cy.url().should('include', '/html')
-        cy.visit('/')
+        cy.url().should('include', '/HTML/question/1')
+      })
+  })
 
+  it('clicking on the "CSS" quiz option navigates to the quiz page', () => {
+    cy.get('[data-cy="testList"]')
+      .find('a')
+      .then((links) => {
         cy.wrap(links[1]).should('contain', 'CSS')
         cy.wrap(links[1]).click()
-        cy.url().should('include', '/css')
-        cy.visit('/')
+        cy.url().should('include', '/CSS/question/1')
+      })
+  })
 
+  it('clicking on the "JavaScript" quiz option navigates to the quiz page', () => {
+    cy.get('[data-cy="testList"]')
+      .find('a')
+      .then((links) => {
         cy.wrap(links[2]).should('contain', 'JavaScript')
         cy.wrap(links[2]).click()
-        cy.url().should('include', '/javascript')
-        cy.visit('/')
+        cy.url().should('include', '/JavaScript/question/1')
+      })
+  })
 
+  it('clicking on the "Accessibility" quiz option navigates to the quiz page', () => {
+    cy.get('[data-cy="testList"]')
+      .find('a')
+      .then((links) => {
         cy.wrap(links[3]).should('contain', 'Accessibility')
         cy.wrap(links[3]).click()
-        cy.url().should('include', '/accessibility')
+        cy.url().should('include', '/Accessibility/question/1')
       })
   })
 })
