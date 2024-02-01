3fe77365773d589ae997be30318e41d3f4b47767
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-02-01 11:02:55 -0600

    remove uncessary only method on test

--- src/features/ThemeToggle/components/ThemeToggle.test.tsx
+++ src/features/ThemeToggle/components/ThemeToggle.test.tsx
@@ -1,11 +1,11 @@
 import { screen, render } from '@test/utilities'
 import { ThemeToggle } from './ThemeToggle'
 
-it.only('it should render the Theme Component', () => {
+it('it should render the Theme Component', () => {
   render(<ThemeToggle />)
 })
 
-it.only('should by default have the light option toggled', () => {
+it('should by default have the light option toggled', () => {
   render(<ThemeToggle />)
 
   const lightOptionInput = screen.getByTestId('lightInput')
@@ -15,7 +15,7 @@ it.only('should by default have the light option toggled', () => {
   expect(darkOptionInput.getAttribute('checked')).toBe(null)
 })
 
-it.only('should toggle the theme to dark when dark option is clicked', async () => {
+it('should toggle the theme to dark when dark option is clicked', async () => {
   const { user } = render(<ThemeToggle />)
   const darkOptionLabel = screen.getByDisplayValue('dark')
   const lightOptionInput = screen.getByTestId<HTMLInputElement>('lightInput')
