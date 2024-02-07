51d079bd3b3cb6269799333fcbc8e8854a54f48c
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-02-06 18:03:36 -0600

    `Question` Desktop
    Adjust HTML structure and CSS to style the `Question` component for
    laptop screens and higher.

--- src/features/Question/components/Question/Question.tsx
+++ src/features/Question/components/Question/Question.tsx
@@ -55,73 +55,76 @@ export const Question = ({
 
   return (
     <>
-      <div className={style.question}>
+      <section className={style.question}>
         <div className={style.questionTitle}>
           <p>{testStatus}</p>
           <h1 data-cy="questionText">{question}</h1>
+
+          <div className={style.progressBar}>
+            <ProgressBar />
+          </div>
         </div>
-        <div className={style.progressBar}>
-          <ProgressBar />
+        <div ref={ref}>
+          <ul data-cy="answerOptions" className={style.answerList}>
+            {choices.map((choice, index) => {
+              return (
+                <li key={choice} className={style.answerChoice}>
+                  <button
+                    data-selected={choice === userChoice}
+                    data-correct={isCorrect !== null && choice === answer}
+                    data-incorrect={
+                      isCorrect === false && choice === userChoice
+                    }
+                    type="button"
+                    onClick={() => {
+                      setUserChoice(choice)
+                      setSelectMessage('')
+                    }}
+                    disabled={isCorrect !== null}
+                  >
+                    <span>{String.fromCharCode(97 + index)}</span>
+                    <p>{choice}</p>
+                    <img src={iconCorrect} alt="" />
+                    <img src={iconIncorrect} alt="" />
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+          <button
+            id="submitOrNext"
+            data-cy="submitOrNext"
+            type="button"
+            className={style.submitButton}
+            onClick={() => {
+              if (isCorrect === true || isCorrect === false) {
+                nextPageNav()
+                setUserChoice(null)
+                setIsCorrect(null)
+              } else if (userChoice === null) {
+                // message to user to select a choice
+                setSelectMessage('Please select an answer')
+              } else if (userChoice === answer) {
+                // message to user that they are correct
+                setIsCorrect(true)
+                updateAnswerSheet(true, userChoice)
+              } else {
+                // message to user that they are incorrect
+                setIsCorrect(false)
+                updateAnswerSheet(false, userChoice)
+              }
+            }}
+          >
+            {isCorrect === null ? 'Submit Answer' : 'Next Question'}
+          </button>
+          {selectMessage ? (
+            <p className={style.selectMessage}>
+              <img src={iconIncorrect} alt="" />
+              {selectMessage}
+            </p>
+          ) : null}
         </div>
-      </div>
-      <div ref={ref}>
-        <ul data-cy="answerOptions" className={style.answerList}>
-          {choices.map((choice, index) => {
-            return (
-              <li key={choice} className={style.answerChoice}>
-                <button
-                  data-selected={choice === userChoice}
-                  data-correct={isCorrect !== null && choice === answer}
-                  data-incorrect={isCorrect === false && choice === userChoice}
-                  type="button"
-                  onClick={() => {
-                    setUserChoice(choice)
-                    setSelectMessage('')
-                  }}
-                  disabled={isCorrect !== null}
-                >
-                  <span>{String.fromCharCode(97 + index)}</span>
-                  <p>{choice}</p>
-                  <img src={iconCorrect} alt="" />
-                  <img src={iconIncorrect} alt="" />
-                </button>
-              </li>
-            )
-          })}
-        </ul>
-        <button
-          id="submitOrNext"
-          data-cy="submitOrNext"
-          type="button"
-          className={style.submitButton}
-          onClick={() => {
-            if (isCorrect === true || isCorrect === false) {
-              nextPageNav()
-              setUserChoice(null)
-              setIsCorrect(null)
-            } else if (userChoice === null) {
-              // message to user to select a choice
-              setSelectMessage('Please select an answer')
-            } else if (userChoice === answer) {
-              // message to user that they are correct
-              setIsCorrect(true)
-              updateAnswerSheet(true, userChoice)
-            } else {
-              // message to user that they are incorrect
-              setIsCorrect(false)
-              updateAnswerSheet(false, userChoice)
-            }
-          }}
-        >
-          {isCorrect === null ? 'Submit Answer' : 'Next Question'}
-        </button>
-        {selectMessage ? (
-          <p className={style.selectMessage}>
-            <img src={iconIncorrect} alt="" />
-            {selectMessage}
-          </p>
-        ) : null}
-      </div>
+      </section>
     </>
   )
 }
