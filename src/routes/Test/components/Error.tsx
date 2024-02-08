b4b71cc0b9044b4d27653e6a2fa0dc6926f86e14
Jorge A Mendoza II <jorgemendozaiidev@gmail.com>
2024-02-08 05:21:12 -0600

    question error page
    Use css modules to style the `Error` component within the `Test` route.

--- src/routes/Test/components/Error.tsx
+++ src/routes/Test/components/Error.tsx
@@ -1,9 +1,10 @@
 import { Link, useRouteError } from 'react-router-dom'
+import style from './error.module.css'
 
 export const TestError = () => {
   const routerError = useRouteError() as Error
   return (
-    <div>
+    <div className={style.error}>
       <h1>Something went wrong!</h1>
       <p>{routerError.message}</p>
 
